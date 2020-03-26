bb974542c28c6fa6cba5a2b47bac55361cb0a94b
destiaina <ina.destia1@gmail.com>
2020-03-27 05:55:45 +0700

    cashflowIna Done

--- app/src/main/java/com/dhanifudin/cashflow/SaveActivity.java
+++ app/src/main/java/com/dhanifudin/cashflow/SaveActivity.java
@@ -1,31 +1,41 @@
 package com.dhanifudin.cashflow;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
-
+import android.widget.TextView;
+import android.widget.Toast;
 import com.dhanifudin.cashflow.models.Transaction;
+import java.io.OptionalDataException;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Optional;
+import static java.util.Optional.*;
 
 public class SaveActivity extends AppCompatActivity {
 
     private EditText descriptionInput;
     private EditText amountInput;
+    private TextView tipe;
     private RadioGroup typeRadioGroup;
     private Transaction item;
     private int index;
+    Locale localeID= new Locale("in", "ID");
+    NumberFormat formatRupiah=NumberFormat.getCurrencyInstance(localeID);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save);
-
         descriptionInput = findViewById(R.id.input_description);
         amountInput = findViewById(R.id.input_amount);
         typeRadioGroup = findViewById(R.id.group_type);
+        tipe=findViewById(R.id.text_type);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -38,8 +48,7 @@ public class SaveActivity extends AppCompatActivity {
             } else if (item.getType() == Transaction.Type.CREDIT) {
                 typeRadioGroup.check(R.id.radio_credit);
             }
-        }
-
+       }
     }
 
     private Transaction.Type getCheckedType() {
@@ -47,23 +56,32 @@ public class SaveActivity extends AppCompatActivity {
             return Transaction.Type.DEBIT;
         } else if (typeRadioGroup.getCheckedRadioButtonId() == R.id.radio_credit) {
             return Transaction.Type.CREDIT;
-        }
+       }
         return Transaction.Type.EMPTY;
     }
 
     public void handleSubmit(View view) {
         String description = descriptionInput.getText().toString();
         int amount = Integer.parseInt(amountInput.getText().toString());
-        Transaction.Type type = getCheckedType();
+       Transaction.Type type = getCheckedType();
 
-        item.setDescription(description);
-        item.setAmount(amount);
-        item.setType(type);
+        if(description.length()==0){
+            descriptionInput.setError("Fill the description");
+        } else if(typeRadioGroup.getCheckedRadioButtonId()==-1){
+            Toast.makeText(getApplicationContext(), "Choose the amount type", Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent();
-        intent.putExtra(MainActivity.TRANSACTION_KEY, item);
-        intent.putExtra(MainActivity.INDEX_KEY, index);
-        setResult(RESULT_OK, intent);
-        finish();
+        } else if(amount==0){
+            amountInput.setError("Fill the amount");
+        }
+        else {
+            item.setDescription(description);
+            item.setAmount(amount);
+            item.setType(type);
+            Intent intent = new Intent();
+            intent.putExtra(MainActivity.TRANSACTION_KEY, item);
+            intent.putExtra(MainActivity.INDEX_KEY, index);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/dhanifudin/cashflow/adapters/TransactionAdapter.java
+++ app/src/main/java/com/dhanifudin/cashflow/adapters/TransactionAdapter.java
@@ -4,19 +4,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.dhanifudin.cashflow.R;
 import com.dhanifudin.cashflow.models.Transaction;
-
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
-public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.ViewHolder>{
 
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.ViewHolder> {
     private List<Transaction> items;
     private OnItemTransactionListener listener;
+    Locale localeID= new Locale("in", "ID");
+    NumberFormat formatRupiah=NumberFormat.getCurrencyInstance(localeID);
+
+    public interface OnItemTransactionListener {
+        void onTransactionClicked(int index, Transaction item);
+    }
 
     public TransactionAdapter(List<Transaction> items, OnItemTransactionListener listener) {
         this.items = items;
@@ -42,24 +47,25 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
         return (items != null) ? items.size() : 0;
     }
 
-    public interface OnItemTransactionListener {
-        void onTransactionClicked(int index, Transaction item);
-    }
-
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView descriptionText;
         TextView amountText;
+        TextView tipeText;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             descriptionText = itemView.findViewById(R.id.text_description);
             amountText = itemView.findViewById(R.id.text_amount);
+            tipeText=itemView.findViewById(R.id.text_type);
         }
+
         public void bind(final int index, final Transaction item) {
             descriptionText.setText(item.getDescription());
-            amountText.setText(String.valueOf(item.getAmount()));
-            // TODO: Tambahkan interaksi click di sini
+            amountText.setText(formatRupiah.format((double)item.getAmount()));
+            tipeText.setText(String.valueOf(item.getType()));
+           // TODO: Tambahkan interaksi click di sini
             itemView.setOnClickListener(new View.OnClickListener() {
+
                 @Override
                 public void onClick(View v) {
                     listener.onTransactionClicked(index, item);
